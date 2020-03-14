dde87090053fadea43243866bf33b20367f85bdd
Malene <maleneklit@hotmail.com>
2020-03-14 13:36:55 +0100

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# trello-clone 
+build after the 'Watch Us Build a Trello Clone' on Vue Mastery
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/ColumnTask.vue
@@ -0,0 +1,61 @@
+<template>
+  <AppDrop
+    @drop="moveTaskOrColumn"
+  >
+    <AppDrag
+      class="task"
+      :transferData="{
+            type: 'task',
+            fromColumnIndex: columnIndex,
+            fromTaskIndex: taskIndex
+          }"
+    >
+          <span
+            class="w-full flex-no-shrink font-bold"
+            @click="goToTask(task)"
+          >
+            {{ task.name }}
+          </span>
+      <p
+        v-if="task.description"
+        class="w-full flex-no-shrink mt-1 text-sm"
+      >
+        {{ task.description }}
+      </p>
+    </AppDrag>
+  </AppDrop>
+</template>
+
+<script>
+import movingTasksAndColumnsMixin from '@/mixins/movingTasksAndColumns'
+import AppDrag from './AppDrag'
+import AppDrop from './AppDrop'
+
+export default {
+  components: { AppDrag, AppDrop },
+  mixins: [movingTasksAndColumnsMixin],
+  props: {
+    task: {
+      type: Object,
+      required: true
+    },
+    taskIndex: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    goToTask (task) {
+      console.log('test');
+      this.$router.push({ name: 'task', params: { id: task.id } })
+    }
+  }
+}
+</script>
+
+
+<style lang="css">
+  .task {
+    @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+  }
+</style>
